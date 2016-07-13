dac47640f88ad9f70994ee294d0eacb56ed6c244
yolomachine <thisisbanana@hotmail.com>
2016-07-13 10:04:10 +0900

    Minor testing changes

--- game.logic.js
+++ game.logic.js
@@ -1,3 +1,45 @@
+<<<<<<< HEAD
+const radius = 17, EPS = 0.000001;
+
+if (gameSession === undefined) var gameSession = {};
+
+gameSession.point = function(x, y, radius) {
+    this.x = x;
+    this.y = y;
+}
+
+gameSession.line = function(p1, p2) {
+    this.beginPoint = p1;
+    this.endPoint = p2;
+    this.beginCoords = gameSession.points[p1];
+    this.endCoords = gameSession.points[p2];
+    this.A = this.endCoords.y - this.beginCoords.y;
+    this.B = this.beginCoords.x - this.endCoords.x;
+    this.C = this.A * this.beginCoords.x + this.B * this.endCoords.y;
+}
+
+function isInSegment(a, b, c) {
+    if (Math.abs(a - b) < EPS || Math.abs(b - c) < EPS) return false;
+    return (a < b && b < c) || (c < b && b < a);
+}
+
+function isIntersecting(a, b) {
+    if (a == b) return false;
+    if (Math.abs(a.A * b.B - b.A * a.B) < EPS) return false;
+    let delta   = a.A * b.B - b.A * a.B;
+    let delta_x = b.B * a.C - a.B * b.C;
+    let delta_y = a.A * b.C - b.A * a.C;
+    x = delta_x / delta;
+    y = delta_y / delta;
+    if ((isInSegment(a.beginCoords.x, x, a.endCoords.x) || isInSegment(a.beginCoords.y, y, a.endCoords.y)) &&
+        (isInSegment(b.beginCoords.x, x, b.endCoords.x) || isInSegment(b.beginCoords.y, y, b.endCoords.y)))
+        return true;
+    return false;
+}
+
+//TODO SOLVER (firebase)
+//TODO RESULTS TABLE
+=======
 const radius = 17;
 
 if (gameSession === undefined) var gameSession = {};
@@ -31,3 +73,4 @@ function checkLineIntersection(a, b) {
     return orientedArea(va, vb) *
            orientedArea(va, vc) < 0;
 }
+>>>>>>> origin/master
--- game.scenery.js
+++ game.scenery.js
@@ -1,3 +1,137 @@
+<<<<<<< HEAD
+const fieldWidth = 1000, fieldHeight = 800;
+var canvas, ctx, cursorPosX, cursorPosY, selectedPoint, count = 0;
+
+if (gameSession === undefined) var gameSession = {};
+gameSession.currentLevel = minLevel;
+
+function initialize() {
+    gameSession.createLayout();
+    cursorPosX = 0; cursorPosY = 0;
+    canvas = document.createElement("canvas");
+    ctx = canvas.getContext("2d");
+    canvas.width = fieldWidth;
+    canvas.height = fieldHeight;
+    document.body.appendChild(canvas);
+    document.body.addEventListener('mousemove',  mouseMove,  false);
+    document.body.addEventListener('mousedown',  mouseDown,  false);
+    document.body.addEventListener('mouseup',    mouseUp,    false);
+    draw();
+}
+
+function movePoint() {
+    gameSession.points[selectedPoint].x = cursorPosX;
+    gameSession.points[selectedPoint].y = cursorPosY;
+    draw();
+}
+
+function selectPoint() {
+    selectedPoint = undefined;
+    let x, y, xDis, yDis, dis, minDis = Math.PI * (radius * radius) / 2;
+    for (let i = 0; i < gameSession.points.length; ++i) {
+        x = gameSession.points[i].x;
+        y = gameSession.points[i].y;
+        xDis = x - cursorPosX;
+        yDis = y - cursorPosY;
+        dis  = xDis * xDis + yDis * yDis;
+        if (dis <= minDis) { minDis = dis; selectedPoint = i };
+    }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, fieldWidth, fieldHeight);
+    let x, y;
+    ctx.lineWidth = 2;
+    for (let i = 0, t0, t1; i < gameSession.edges.length; ++i) {
+        t0 = gameSession.points[gameSession.edges[i].beginPoint];
+        t1 = gameSession.points[gameSession.edges[i].endPoint];
+        ctx.lineJoin = ctx.lineCap = 'round';
+        ctx.shadowBlur = 15;
+        ctx.shadowColor = 'rgb(0, 0, 0)';
+        ctx.strokeStyle = '#98FB98';
+        for (let j = 0; j < gameSession.edges.length; ++j) {
+            let curEdge = gameSession.edges[i], nextEdge = gameSession.edges[j];
+            if (isIntersecting(curEdge, nextEdge)) {
+                ctx.strokeStyle = '#e74c3c';
+                ++count;
+                break;
+            }
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(t0.x, t0.y);
+        ctx.lineTo(t1.x, t1.y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    for (let i = 0; i < gameSession.points.length; ++i) {
+        x = gameSession.points[i].x;
+        y = gameSession.points[i].y;
+        drawPointPath(radius, x, y);
+        ctx.fillStyle = '#333';
+        ctx.fill();
+    }
+}
+
+function drawPointPath(r, x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI*2, true);
+    ctx.closePath();
+}
+
+function getMouseCoords(event) {
+    cursorPosX = event.pageX - canvas.offsetLeft;
+    cursorPosY = event.pageY - canvas.offsetTop;
+    if (cursorPosX > fieldWidth - radius) cursorPosX = fieldWidth - radius;
+    else if (cursorPosX < radius) cursorPosX = radius;
+    if (cursorPosY > fieldHeight - radius) cursorPosY = fieldHeight - radius;
+    else if (cursorPosY < radius) cursorPosY = radius;
+}
+
+function mouseDown(event) {
+    getMouseCoords(event);
+    selectPoint();
+    movePoint();
+}
+
+function mouseUp(event) {
+    selectedPoint = undefined;
+    if (count == 0) { alert("You won"); gameSession.changeLevel(1) };
+    draw();
+}
+
+function mouseMove(event) {
+    count = 0;
+    getMouseCoords(event);
+    if (selectedPoint != undefined) movePoint();
+}
+
+function clear(obj) {
+    while (obj.lastChild) obj.removeChild(obj.lastChild);
+}
+
+gameSession.changeLevel = function(inc) {
+    if (gameSession.currentLevel + inc >= minLevel && gameSession.currentLevel + inc <= maxLevel)
+        gameSession.currentLevel += inc;
+    gameSession.createLayout();
+}
+
+gameSession.createLayout = function() {
+    gameSession.points = [];
+    gameSession.edges  = [];
+    let level = presetLevels[gameSession.currentLevel];
+    for (let i = 0; i < level.points.length; ++i)
+        gameSession.points.push(new gameSession.point(level.points[i].x, level.points[i].y, radius));
+    for (let i = 0; i < level.edges.length; ++i)
+        for (let j = 0; j < level.edges[i].length - 1; ++j)
+            gameSession.edges.push(new gameSession.line(level.edges[i][j], level.edges[i][j + 1]));
+};
+
+gameSession.generateLayout = function(level) {
+
+}
+
+=======
 const fieldWidth = 1000, fieldHeight = 800;
 var canvas, ctx, cursorPosX, cursorPosY, selectedPoint;
 
@@ -108,4 +242,5 @@ gameSession.generateLayout = function(level) {
 
 }
 
+>>>>>>> origin/master
 window.onload = initialize;
